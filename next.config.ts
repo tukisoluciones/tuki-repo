e55d4d0a1a4c182b6f3e40e6db15b30588267ac5
tukisoluciones <tukisoluciones@gmail.com>
2026-07-29 21:13:28 -0300

    Habilita static export para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
+    output: "export",
+    trailingSlash: true,
+    images: {
+          unoptimized: true,
+    },
 };
 
 export default nextConfig;
